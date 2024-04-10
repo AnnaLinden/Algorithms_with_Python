0226a63fc16d31c977a487b13e1787de17bed928
AnnaLinden <linden.a.anna@gmail.com>
2024-04-10 17:18:58 +0300

    chapter 1 kysymys 2

--- chapter_1_python_data_types_and_structures.py
+++ chapter_1_python_data_types_and_structures.py
@@ -10,3 +10,32 @@ sorted_string = ''.join(sorted(input_string))
 
 #print the sorted string
 print(sorted_string)
+
+""" Kysymys 2
+Write a program that takes two integers, a and b, as input.
+Your program should compute and display:
+The sum of a and b
+The difference when b is subtracted from a
+The product of a and b
+The quotient when a is divided by b
+The remainder when a is divided by b
+The result of  """
+
+a = int(input())
+b = int(input())
+
+# Perform arithmetic operations
+sum_ab = a + b
+difference_ab = a - b
+product_ab = a * b
+quotient_ab = a / b
+remainder_ab = a % b
+exponent_ab = a ** b
+
+# Display the results
+print(f"{a} + {b} is {sum_ab}")
+print(f"{a} - {b} is {difference_ab}")
+print(f"{a} * {b} is {product_ab}")
+print(f"{a} / {b} is {quotient_ab}")
+print(f"{a} % {b} is {remainder_ab}")
+print(f"{a} ^ {b} is {exponent_ab}")
